0195fd4af23229d0f5097abad6df8f2041b19e15
ros2ott <ros2ott@gmail.com>
2022-04-05 14:59:55 +0900

    tried querySelector method to access to DOM objects

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,18 @@
 "use strict"
 
-console.log("hello");
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("#btn-login");
 
-console.log("bye");
+
+loginBtn.addEventListener("click", login);
+
+// ecma 화살표 사용시 절차적으로 읽어 해당 login 함수의 존재를 모르겠다고 함
+function login() {
+
+    console.log(id.value);
+    const req = {
+        id : id.value,
+        password : password.value
+    };
+}
