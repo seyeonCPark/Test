a6e0db4a1197de03de94469f4a0b32ba7151fdee
ros2ott <ros2ott@gmail.com>
2022-04-05 16:17:48 +0900

    tried to fatch the request data

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 // modules
 const express = require('express');
 const app = express();
-const PORT = 3000;
+// const PORT = 3000;
 
 // routing
 const home = require('./src/routes/home');
@@ -12,14 +12,15 @@ const home = require('./src/routes/home');
 app.set('views', './src/views');
 app.set('view engine', "ejs"); // html소스 해석엔진
 
-// 라우팅
-app.use("/", home); // middleware를 등록하는 메소드 use
-
-//
+// middleware를 등록하는 메소드 use
 app.use(express.static(`${__dirname}/src/public`));
 
-module.exports = app;
+app.use(express.json()); // json data를 파싱해올 수 있도록
+app.use(express.urlencoded({extended : true})); // URL에 한글,공백이 있을 경우 제대로 인식되지 않는 문제를 방지
 
+app.use("/", home);
+
+module.exports = app;
 
 /* 
 const http = require("http");
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,7 +6,8 @@ const router = express.Router(); // front controller
 const ctrl = require('./home.ctrl'); // controller
 
 // 요청한 경로로 이동시켜주는 라우팅 기능
-router.get('/', ctrl.home);
-router.get('/login', ctrl.login);
+router.get('/', ctrl.output.home);
+router.get('/login', ctrl.output.login);
+router.post('/login', ctrl.process.login);
 
 module.exports = router;
